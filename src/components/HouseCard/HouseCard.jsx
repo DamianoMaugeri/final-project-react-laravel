ee8bc8b586d312ac7f030d3a58bebe8b9c2e2e57
DamianoMaugeri <damianomaugeri97@icloud.com>
2025-05-20 21:19:01 +0200

    creata la base

---
+++ src/components/HouseCard/HouseCard.jsx
@@ -0,0 +1,54 @@
+import { useMatch } from "react-router-dom";
+import HeartButton from "../Heart Button/HeartButton";
+import style from './HouseCard.module.css'
+import placeHolder from '../../assets/placeholder.png'
+
+
+
+export default function HouseCard({ content }) {
+
+    const { id, title, image, director, year , like} = content
+
+    //const isOwnerPage = useMatch("/owners/:id");
+
+
+
+    return (
+
+        <div className={`card h-100 ${style.hover}`} >
+            {/* immagine */}
+            <div className="position-relative">
+                <img
+                    src={image}
+                    onError={(e) => {
+                        e.target.onerror = null; // se la immagine e innacesibile 
+                        e.target.src = placeHolder; // metti il placeholder
+                    }}
+                    className={`card-img-top ${style.card_img}`}
+                    alt="movie"
+                />
+                {/* Cuoricino */}
+                {/* {!isOwnerPage && <HeartButton vote={vote} id={id} />} */}
+                 <HeartButton vote={like} id={id} />
+            </div>
+
+            {/* contenuto della card */}
+            <div className="card-body">
+                <div className="d-flex justify-content-between">
+                    <div>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{director}</p>
+
+                    </div>
+                    {/* <div>
+                        <h5>{price_per_day.toString().replace('.', ',')} &#x20AC;</h5>
+                        <p>giorno</p>
+
+                    </div> */}
+                </div>
+                <p className="card-text">{year}</p>
+            </div>
+        </div>
+
+    )
+}
---
+++ src/pages/ShowPage/ShowPage.jsx
@@ -0,0 +1,85 @@
+import HouseShowCard from "../../components/House Show Card/HouseShowCard";
+import ReviewCard from "../../components/Review Card/ReviewCard";
+import ReviewForm from "../../components/ReviewForm/ReviewForm";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import style from "./ShowPage.module.css";
+import Loader from "../../components/Loader/Loader";
+//import EmailForm from "../../components/emailForm/emailForm";
+import HeaderMain from "../../components/HeaderMain/HeaderMain";
+//import RentForm from "../../components/RentForm/RentForm";
+
+export default function ShowPage() {
+    // const [house, setHouse] = useState(null);
+    // const [reviewBoolean, setReviewBoolean] = useState(false);
+    // const [emailBoolean, setEmailBoolean] = useState(false);
+    // const [rentBoolean, setRentBoolean] = useState(false);
+    // const { slug } = useParams();
+
+     const { id} = useParams();
+     const [movie, setMovie] = useState(null);
+     const [reviewBoolean, setReviewBoolean] = useState(false);
+
+
+
+
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/api/movies/${id}`)
+            .then(res => setMovie(res.data.data))
+            .catch(err => console.error(err));
+    }, [id]);
+
+    return (
+        <div className={`d-flex flex-column flex-grow-1 mt-5 pt-5 ${style.showPageContainer}`}>
+            <HeaderMain />
+            {movie ? (
+                <>
+                    <HouseShowCard movieEl={movie} />
+
+
+                    <div className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-3 mb-5 mt-5">
+                        {/* <button
+                            className={`${style.btn} ${emailBoolean ? style['btn-active'] : ''}`}
+                            onClick={() => {
+                                setReviewBoolean(false);
+                                setRentBoolean(false);
+                                setEmailBoolean(!emailBoolean);
+                            }}>
+                            Contatta il proprietario
+                        </button> */}
+
+                        {/* <button
+                            className={`${style.btn} ${rentBoolean ? style['btn-active'] : ''}`}
+                            onClick={() => {
+                                setReviewBoolean(false);
+                                setEmailBoolean(false);
+                                setRentBoolean(!rentBoolean);
+                            }}>
+                            Prenota adesso
+                        </button> */}
+
+                        <button
+                            className={`${style.btn} ${reviewBoolean ? style['btn-active'] : ''}`}
+                            onClick={() => {
+                                setReviewBoolean(!reviewBoolean);
+                            }}>
+                            Lascia una recensione
+                        </button>
+                    </div>
+
+
+                    {reviewBoolean && <ReviewForm id={movie.id} />}
+
+
+                    <hr />
+                     <ReviewCard reviews={movie.reviews} /> 
+                </>
+            ) : (
+                <div className="d-flex align-items-center justify-content-center flex-grow-1">
+                    <Loader />
+                </div>
+            )}
+        </div>
+    );
+}
